6f3a08309228d8603ffd3905695bd2d212e04dcc
VictoriaReyes <victoria.jaime@silverstone-tech.com>
2025-11-14 14:04:45 -0600

    camina

---
+++ bunkerterminal/Assets/scripts/Player.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        // SOLO flechas
+        if (Input.GetKey(KeyCode.UpArrow))
+            moveY = 1f;
+        if (Input.GetKey(KeyCode.DownArrow))
+            moveY = -1f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1f;
+        if (Input.GetKey(KeyCode.RightArrow))
+            moveX = 1f;
+
+        input = new Vector2(moveX, moveY).normalized;
+
+        // DEBUG: ver lo que está leyendo
+        if (input != Vector2.zero)
+        {
+            Debug.Log("Input: " + input);
+            transform.Translate(input * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = input * moveSpeed;
+    }
+
+
+//     void OnCollisionEnter2D(Collision2D collision)
+// {
+//     if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+//     {
+//         // Aquí sabes que chocaste con una pared
+//         // Ejemplo: reproducir sonido, partículas, etc.
+//         Debug.Log("Choqué con pared");
+//     }
+// }
+
+}
